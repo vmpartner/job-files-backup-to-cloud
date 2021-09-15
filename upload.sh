572bc0f2d577396f5fb39ec2bff43555fd661f40
vitams <vmpartner@gmail.com>
2021-09-15 17:10:49 +0500

    Up

--- upload.sh
+++ upload.sh
@@ -13,7 +13,8 @@ else
   echo "Start $(date '+%d-%m-%Y %H:%M:%S')"
   FILESIZE=$(stat -c%s /${SRC})
   echo "Size of ${SRC} = $FILESIZE bytes."
-  rclone copy /source "$RCLONE_DEST" "$RCLONE_PARAMS"
+  echo "rclone copy /source $RCLONE_DEST $RCLONE_PARAMS"
+  rclone copy /source $RCLONE_DEST $RCLONE_PARAMS
   if [ "${CHECK_URL}" = "**None**" ]; then
     echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
   else
